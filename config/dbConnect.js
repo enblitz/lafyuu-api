52064accfeb4b38fe91ab7c8ddd633fad4d3313b
Divyarajsinh01 <divyarajsinhrayjada3@gmail.com>
2024-03-13 09:08:47 +0530

    varibale name changes

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
-const sequelize = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+const sequelize = new Sequelize(process.env.MYSQL_LF_DB_NAME, process.env.MYSQL_LF_USERNAME, process.env.MYSQL_LF_PASSWORD, {
   host: process.env.MYSQL_DB_SERVER,
   dialect: 'mysql',
   logging: false
